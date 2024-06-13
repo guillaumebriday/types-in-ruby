a6c03127e138ee029718db995ea4ab4d7ded84ee
Guillaume Briday <guillaume.briday@per-angusta.com>
2024-06-13 16:03:58 +0200

    wip

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+
+class Dog {
+  public function speak() {
+    echo "whoaf";
+  }
+}
+
+class Cat {
+  public function speak() {
+    echo "miaou";
+  }
+}
+
+function speak(Dog|Cat $animal): void {
+  $animal->speak();
+}
+
+speak(new Dog);
+echo "\n";
+speak(new Cat);
+
+
+
+
+
+echo "\n";
